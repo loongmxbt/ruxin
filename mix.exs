653c59b4426b4c2f20c20de9a1dd9ab7c1530fe9
loongmxbt <loongmxbt@gmail.com>
2018-04-20 15:09:11 +0800

    add exadmin

--- mix.exs
+++ mix.exs
@@ -19,7 +19,8 @@ defmodule Ruxin.Mixfile do
   def application do
     [mod: {Ruxin, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex,
+                    :ex_admin]]
   end
 
   # Specifies which paths to compile per environment.
